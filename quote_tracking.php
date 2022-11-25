43c8c47e11687933097ad16e404096abb140e21b
npdhung <89493286+npdhung@users.noreply.github.com>
2022-11-25 15:44:44 -0600

    Connect to legacy database to get customer info

--- quote_tracking.php
+++ quote_tracking.php
@@ -15,10 +15,41 @@
     </nav>
     <hr>
     <?php
+    $dbname = "mysql:host=blitz.cs.niu.edu:3306;dbname=csci467";
+    $user = "student";
+    $pass = "student";
+    try { // connect to the database
+        $pdo = new PDO($dbname, $user, $pass);
+
         echo "<br>";
-        echo "<h3>Implement Quote Tracking.</h3>";
-        echo "<h4>Description...</h4>";
+        echo "<h3>Associate creates new Quote.</h3>";
+        echo "<h4>Plan Repair Services Portal. Username: xxxx</h4>";
+        echo "<h5>Create new quote for Customer:</h5>";
+        
+        echo "<form action=\"quote_tracking.php\" method = GET>";
+        echo "<label for='Name'>Select customer: </label>";
+        echo "<select id='Name' name='pid'>";
+        $res = $pdo->query("SELECT name, id FROM customers");
+        while($fet = $res->fetch(PDO::FETCH_ASSOC)){
+              $name = $fet["name"];
+              $pid = $fet["id"];
+              echo "<option value=".$pid.">".$name."</option>";
+        }
+        echo "</select>";
+        echo " <input type='submit' value='New Quote'> </form>";
+        
+        $res = $pdo->query("SELECT COUNT(*) FROM customers");
+        $count = $res->fetchColumn();
+        echo "$count current customers.";
+        echo "<h5>List of current quotes for user xxxx:</h5>";
         echo "<br>";
+        echo "<h5> quotes found.</h5>";
+        echo "<br>";
+
+    }
+    catch(PDOexception $e) { // handle that exception
+        echo "Connection to database failed: " . $e->getMessage();
+    }
     ?>
 </body>
 </html>
