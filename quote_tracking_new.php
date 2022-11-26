987bfe108c201adf53893718287edb7adf0d3733
npdhung <89493286+npdhung@users.noreply.github.com>
2022-11-25 23:34:50 -0600

    Initial commit for the page of adding new quote

---
+++ quote_tracking_new.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Quote System - Group 2B</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+    <h1>Quote System</h1>
+    <h2>Quote Tracking - Add Quote</h2>
+    <nav>
+        <ul>
+            <li><a href="main.php">Main Page</a></li>
+            <li><a href="quote_tracking.php">Quote Tracking</a></li>
+        </ul>
+    </nav>
+    <hr>
+    <?php
+        echo "<br>";
+        echo "<h3>Implement Adding new Quote.</h3>";
+        echo "<h4>Click on the Quote Tracking link above to go the Quote
+        Tracking page to add or edit another quote.</h4>";
+        echo "<br>";
+    ?>
+</body>
+</html>
