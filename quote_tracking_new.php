dea24a4961c2e58efb8f7daf28352a0793cad63b
npdhung <89493286+npdhung@users.noreply.github.com>
2022-11-26 23:09:24 -0600

    Add information of customer in the new quote

--- quote_tracking_new.php
+++ quote_tracking_new.php
@@ -7,7 +7,7 @@
     </head>
 <body>
     <h1>Quote System</h1>
-    <h2>Quote Tracking - Add Quote</h2>
+    <h2>Quote Tracking - Add New Quote</h2>
     <nav>
         <ul>
             <li><a href="main.php">Main Page</a></li>
@@ -16,11 +16,34 @@
     </nav>
     <hr>
     <?php
+    $dbname = "mysql:host=blitz.cs.niu.edu:3306;dbname=csci467";
+    $user = "student";
+    $pass = "student";
+    try { // connect to the database
+        
+        $pdo = new PDO($dbname, $user, $pass);
+        $id = $_GET["pid"];
+        $res = $pdo->query("SELECT name, street, city, contact FROM customers WHERE id = $id");
+        
+        $fet = $res->fetch(PDO::FETCH_ASSOC);
+        $name = $fet["name"];
+        $street = $fet["street"];
+        $city = $fet["city"];
+        $contact = $fet["contact"];
+        
         echo "<br>";
-        echo "<h3>Implement Adding new Quote.</h3>";
+        echo "<h3>New Quote for: $name.</h3>";
+        echo "$street<br>$city<br>$contact";
+        
         echo "<h4>Click on the Quote Tracking link above to go the Quote
         Tracking page to add or edit another quote.</h4>";
         echo "<br>";
+
+
+    }
+    catch(PDOexception $e) { // handle that exception
+        echo "Connection to database failed: " . $e->getMessage();
+    }
     ?>
 </body>
 </html>
