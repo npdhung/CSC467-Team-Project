e48fa73c96099cd9a9f2ff7aafd4a7600ced7b7a
Tervon <77021628+FateVoid@users.noreply.github.com>
2022-12-02 13:24:09 -0600

    Update quote_processing.php
    fixed Cred.

--- quote_processing.php
+++ quote_processing.php
@@ -21,15 +21,15 @@ session_start();
     $dbname = "mysql:host=blitz.cs.niu.edu:3306;dbname=csci467";
     $user = "student";
     $pass = "student";
-    
-    $local_dbname = "mysql:host=courses;dbname=z1924897";
-    $lc_user = "z1924897";
-    $lc_pass = "1979Jan05";
+
+    // Get the credentials to connect to local server or hopper
+    include 'local_cred.php';
+
     try { // connect to the database
         $pdo = new PDO($dbname, $user, $pass);
         $pdo_local = new PDO($local_dbname, $lc_user, $lc_pass);
         print_r($_SESSION);
-        
+
         echo "<br>";
         echo "<h3>Associate Finalizing Quote: Add discount (% and amount),
             contact email, note, and generate total price.</h3>";
@@ -37,12 +37,12 @@ session_start();
         $last = $_SESSION["assoc_last"];
         echo "<h4>Plan Repair Services Portal welcomes Associate $first $last
           </h4>";
-        
+
         echo "<form action=\"quote_finalizing.php\" method = GET>";
-        
+
         echo "<label for='Name'>Select Quote ID to Process: </label>";
         echo "<select id='Name' name='qid'>";
-        $res = $pdo_local->query("SELECT Id, Status FROM Quotes 
+        $res = $pdo_local->query("SELECT Id, Status FROM Quotes
             WHERE Status = 'in-progress'");
         while($fet = $res->fetch(PDO::FETCH_ASSOC)){
               $name = $fet["Status"];
@@ -52,7 +52,7 @@ session_start();
         echo "</select>";
         echo " <input type='submit' value='Select'> </form>";
         echo "(Can only start Finalizing Quotes that are in-progress status)";
-        
+
         if (isset($_GET["qid"])) {
             $qid = $_GET["qid"];
             echo $qid;
@@ -64,15 +64,15 @@ session_start();
             // $res = $pdo_local->exec("DELETE FROM Quotes
             // WHERE Id = $qid;");
         }
-        
+
         echo "<h4>List of current quotes for Associate $first $last:</h4>";
-        
+
         $assoc_id = $_SESSION["assoc_id"];
         $pdo_local = new PDO($local_dbname, $lc_user, $lc_pass);
         // ItemNumber -> AssocId
-        $res = $pdo_local->query("SELECT Id, Status FROM Quotes 
+        $res = $pdo_local->query("SELECT Id, Status FROM Quotes
             WHERE AssociateId = $assoc_id");
-        
+
         echo "<table border=0 cellpadding=5 align=center>";
         echo "<tr><th>Quote ID</th><th>Status</th></tr>";
         while($fet = $res->fetch(PDO::FETCH_ASSOC)){
